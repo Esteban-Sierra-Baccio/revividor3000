81284986d116e0f60d033475e897ae8053e9bfeb
Esteban-Sierra-Baccio <110852087+Esteban-Sierra-Baccio@users.noreply.github.com>
2023-11-13 20:14:07 -0600

    Update route.js

--- API/routes/route.js
+++ API/routes/route.js
@@ -1,26 +1,37 @@
 const express = require('express');
-const tempController = require('../controllers/tempController');
-const humController = require('../controllers/humController');
-const segController = require('../controllers/segController');
-const segController2 = require('../controllers/segController2')
 const router = express.Router();
 
 // Ruta 1: temperatura digital
+const tempDigController = require('../controllers/tempDigController');
+router.get('/api/getLogs/:deviceID', tempDigController.getLogs);
+router.post('/api/logTemp/:deviceID/:temperature', tempDigController.logTemp);
+
 // Ruta 2: Temperatura ambiental
+const tempController = require('../controllers/tempController');
+const humController = require('../controllers/humController');
 router.get('/api/getLogs/:deviceID', tempController.getLogs);
 router.post('/api/logTemp/:deviceID/:temperature', tempController.logTemp);
 router.get('/api/getLogs/:deviceID', humController.getLogs);
 router.post('/api/logHum/:deviceID/:humedad', humController.logHum);
+
 // Ruta 3: seguidor 1
+const segController = require('../controllers/segController');
 router.get('/api/getLogsSeg/:device_id', segController.getLogsSeg);
 router.post('/api/logSegLinea/:device_id/:dato_linea', segController.logSegLinea);
+
 // Ruta 4: seguidor 2
+const segController2 = require('../controllers/segController2');
 router.get('/api/getLogsSeg2/:device_id', segController2.getLogsSeg2);
 router.post('/api/logSegLinea2/:device_id/:dato_linea', segController2.logSegLinea2);
-// Ruta 5: RFID
-router.get('/api/getLogs/:device_id', rfidController.getLogs);
-router.post('/api/logtag/:device_id/:dato_linea', rfidController.logtag);
-// Ruta 6: color
 
+// Ruta 5: RFID
+const rfidController = require('../controllers/rfidController')
+router.get('/api/getLogs/:device_id', rfidController.getLogs);
+router.post('/api/logtag/:device_id/:tag', rfidController.logtag);
+
+// Ruta 6: color
+const colorController = require('../controllers/colorController')
+router.get('/api/getLogs/:deviceID', colorController.getLogs);
+router.post('/api/logTemp/:deviceID/:temperature', colorController.logTemp);
 
 module.exports = router;
